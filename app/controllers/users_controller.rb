c79bd892dc67c797ac6266a6707eddb3fbf07636
NTD1995 <76642241+NTD1995@users.noreply.github.com>
2021-02-01 17:12:23 +0900

    ユーザー編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,13 @@ class UsersController < ApplicationController
   end
 
   def update
-    current_user.update(user_params)
-  end
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end 
+  
 
   private
 
